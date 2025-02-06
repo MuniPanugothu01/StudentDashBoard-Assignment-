9ce5c728792a028ec02faa74afac085f355bccfc
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-02-06 07:14:33 +0530

    data displated in table formate

--- src/Components/ClassData.jsx
+++ src/Components/ClassData.jsx
@@ -4,32 +4,34 @@ import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 
 const ClassData = () => {
+  let ClassData = Data.map((val, index) => {
+    return (
+      <tr key={index}>
+        <td>{val.id}</td>
+        <td>{val.ClassName}</td>
+        <td>{val.StaffName}</td>
+        <td>{val.TodayTime}</td>
+        <td>{val.BookNow}</td>
+      </tr>
+    );
+  });
+
   return (
     <>
       {console.log(Data)}
-      {Data.map((val, index) => {
-        return (
-          <div key={index}>
-            <Card style={{ width: "20rem", height: "23rem" }}>
-              <Card.Img variant="top" src={val.image} height={150} />
-              <Card.Body>
-                <Card.Title style={{ color: "red" }}>
-                  Class: <span style={{ color: "black" }}>{val.ClassName}</span>
-                </Card.Title>
-                <h6>
-                  <span style={{ color: "red" }}>Staff-Name:</span>{" "}
-                  {val.StaffName}
-                </h6>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-              </Card.Body>
-            </Card>
-          </div>
-        );
-      })}
+
+      <table>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>ClassName</th>
+            <th>StaffName</th>
+            <th>BookNow</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>{ClassData}</tbody>
+      </table>
     </>
   );
 };
